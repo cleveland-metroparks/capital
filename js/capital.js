76c4cb9b36bea9da8b5fd8f4896f8205b712db97
Brandon Garman <bdg1@clevelandmetroparks.com>
2014-10-15 09:53:07 -0400

    data update

--- js/capital.js
+++ js/capital.js
@@ -17,7 +17,7 @@ var geoJson = {
           41.30404603419457
         ]
       }
-    },
+    }/*,
     {
       "type": "Feature",
       "properties": {
@@ -68,54 +68,25 @@ var geoJson = {
           41.41041955746075
         ]
       }
-    }
+    }*/
   ]
 }
 
 map.markerLayer.setGeoJSON(geoJson)
 
-	/*	// Add custom popup html to each marker
+/*		// Add custom popup html to each marker
 		var markers = map.markerLayer.on('layeradd', function(e) {
 			var marker = e.layer;
 			var feature = marker.feature; 
 			var images = feature.properties.images
-			var slideshowContent = '';
 
-			for(var i = 0; i < images.length; i++) {
-				var img = images[i];
-
-				slideshowContent += '<div class="image' + (i === 0 ? ' active' : '') + '">' +
-									  '<img src="' + img[0] + '" />' + '</div>';
-			}
 			
 			
 			
 		// Create custom popup content
 		// Javascript is writing html in the popup window to populate features dynamically
-			var popupContent =  '<div id="' + feature.properties.id + '" class="popup">' +
-									'<h2>' + feature.properties.Location + '</h2>' +
-									'<div class="slideshow">' +
-										slideshowContent +
-									'</div>' +
-									'<div class="cycle">' +
-										'<a href="#" class="prev" onclick="return moveSlide(\'prev\')">&laquo; Previous</a>' +
-										'<a href="#" class="next" onclick="return moveSlide(\'next\')">Next &raquo;</a>' +
-									'</div>' +
-									'<h3>' + "Bank Number" + " " + feature.properties.bank_num + '</h3>' +
-									'<table>' + 
-										'<tr>' + '<th>' + " " + '</th>' + '<th>' + "Score" + '</th>' + '<th>' + "Value" + '</th>' + '</tr>' +
-										'<tr class="odd_tr">' + '<td>' + "Total Score (BEHI)"  + '</td>' + '<td>' + feature.properties.tot_score  + '</td>' + '</tr>' +
-										'<tr>' + '<td>' + "Rank"  + '</td>' + '<td>' + feature.properties.rank  + '</td>' + '</tr>' +
-										'<tr class="odd_tr">' + '<td>' + "Bank Height (ft)"  + '</td>' + '<td>' + feature.properties.height  + '</td>' + '</tr>' +
-										'<tr>' + '<td>' + "Bank Length (ft)"  + '</td>' + '<td>' + feature.properties.length  + '</td>' + '</tr>' +
-										'<tr class="odd_tr">' + '<td>' + "Materials Score"  + '</td>' + '<td>' + feature.properties.mat_score  + '</td>' + '</tr>' +
-										'<tr>' + '<td>' + "Stratification Score"  + '</td>' + '<td>' + feature.properties.strat_scor  + '</td>' + '</tr>' +
-										'<tr class="odd_tr">' + '<td>' + "Root Score"  + '</td>' + '<td>' + feature.properties.root_score  + '</td>' + '<td>' + feature.properties.root_val  + '</td>' + '</tr>' +
-										'<tr>' + '<td>' + "Density Score"  + '</td>' + '<td>' + feature.properties.dens_score  + '</td>' + '<td>' + feature.properties.dens_val  + '</td>' + '</tr>' +
-										'<tr class="odd_tr">' + '<td>' + "Angle Score"  + '</td>' + '<td>' + feature.properties.angle_scor  + '</td>' + '<td>' + feature.properties.angle_val + '</td>' + '</tr>' +
-										'<tr>' + '<td>' + "Surface Score"  + '</td>' + '<td>' + feature.properties.surf_score  + '</td>' + '<td>' + feature.properties.surf_val  + '</td>' + '</tr>' +
-									'</table>' +
-								'</div>';
+			var popupContent =  '<div class="popup">' +
+									'<p>' + feature.properties.project + '</p>' + '</div>';
 
 		// http://leafletjs.com/reference.html#popup
 		// Bind the popups to their corresponding data point
@@ -127,32 +98,9 @@ map.markerLayer.setGeoJSON(geoJson)
 
 		map.markerLayer.setGeoJSON(geoJson)
 
-
-		// This example uses jQuery to make selecting items in the slideshow easier
-		// Creates the functionality for the next/prev buttons for the image slideshow
-		function moveSlide(direction) {
-			var $slideshow = $('.slideshow'),
-				totalSlides = $slideshow.children().length;
-
-			if (direction === 'prev') {
-				var $newSlide = $slideshow.find('.active').prev();
-				if ($newSlide.index() < 0) {
-					$newSlide = $('.image').last();
-				}
-			} else {
-				var $newSlide = $slideshow.find('.active').next();
-				if ($newSlide.index() < 0) {
-					$newSlide = $('.image').first();
-				}
-			}
-
-			$slideshow.find('.active').removeClass('active').hide();
-			$newSlide.addClass('active').show();
-			return false;
-		}
-
-
 */
+
+
 
 // Add layers to turn on/off
 // Cleveland Metroparks trails app base layer
